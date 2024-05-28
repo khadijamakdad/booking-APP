cec732dff9ea4a1d806c425e653bff439f02b882
Khadija Makdad <khadijamakdad290@gmail.com>
2024-05-28 21:08:26 +0200

    message

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hotel MK</title>
+    <!-- Utiliser Bootstrap avec un thème personnalisé -->
+    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/slate/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5">
+        @yield('content')
+    </div>
+</body>
+</html>
